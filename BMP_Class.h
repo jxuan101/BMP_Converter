7f3042f1b313608d7e10ee455e79cfb7786debc4
jxuan101 <35944722+jxuan101@users.noreply.github.com>
2022-06-02 01:49:33 -0400

    Add parse logic for bmp file header

--- BMP_Class.h
+++ BMP_Class.h
@@ -1,14 +1,22 @@
 #ifndef BMP_CLASS_H_
 #define BMP_CLASS_H_
 
+#include <fstream>
+#include <iostream>
+#include <string>
 #include <vector>
 
+const uint16_t kBmpHex = 0x4D42;
+const uint16_t kBitsPerByte = 8;
+const uint16_t kBitsPerPixel = 24;
+const uint32_t kCompression = 0;
+
 struct BitmapFileHeader {
-  uint16_t file_type{0x4D42};             // File type, must be 0x4D42 which is 'BM'
+  uint16_t file_type{0x0000};             // File type, must be 0x4D42 which is 'BM'
   uint32_t file_size{0};                  // Size of the BMP file (bytes)
   uint16_t app_reserved{0};               // Reserved, will always be 0
   uint16_t app_reserved2{0};              // Reserved, will always be 0
-  uint32_t bitmap_offset{0};              // Starting position of the pixel array
+  uint32_t data_offset{0};              // Starting position of the pixel array
 };
 
 struct BitmapInfoHeader {
@@ -23,25 +31,6 @@ struct BitmapInfoHeader {
   int32_t vertical_resolution{0};         // Pixel per meter vertically
   uint32_t colors{0};                     // Number of colors in the color palette. 0 = 2^n colors
   uint32_t colors_important{0};           // Number of important colors used, 0 when every color is important
-};
-
-class BMP {
-  public:
-    BitmapFileHeader file_header;
-    BitmapInfoHeader info_header;
-    std::vector<uint8_t> bitmap_data;
-      
-    BMP(const char *file_name) {
-      read(file_name);
-    }
-
-    void read(const char *file_name) {
-
-    }
-
-    void write(const char* file_name) {
-
-    }
 };
 
 #endif // BMP_CLASS_H_
