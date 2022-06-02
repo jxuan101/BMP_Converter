03c0e4e2da61a71ed7033a196ec7cbe1bca3d870
jxuan101 <35944722+jxuan101@users.noreply.github.com>
2022-06-02 01:45:30 -0400

    Add input prompt for bmp file

--- BMP_Converter.cpp
+++ BMP_Converter.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <string>
 
 #include "BMP_Class.h"
 
 
 int main() {
-
-  std::cout << "Hi" << std::endl;
-
+  std::string file_name = "";
+  std::cout << "Input file name (Must be in BMP format, 24 bits per pixel, uncompressed):\n" << std::endl;
+  std::cin >> file_name;
+  try {
+    BMP bmp(file_name);
+  }
+  catch (const std::runtime_error &err) {
+    std::cerr << err.what() << std::endl;
+    // std::cin >> file_name;
+  }
 }
