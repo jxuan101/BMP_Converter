6e54eb4c38191314d6fe9c4e48dc56dd1a4d791e
jxuan101 <35944722+jxuan101@users.noreply.github.com>
2022-06-03 00:20:13 -0400

    Remove comment in App.cpp

--- App.cpp
+++ App.cpp
@@ -1,6 +1,5 @@
 #include <wx/wx.h>
 
-#include "BMP.h"
 #include "App.h"
 #include "MainFrame.h"
 
@@ -8,13 +7,6 @@ IMPLEMENT_APP(App);
 
 bool App::OnInit() {
   MainFrame *main_frame = new MainFrame("BMP Photo Negative Filter");
-  //try {
-  //  BMP bmp(main_frame->GetCurrentDocPath());
-  //  bmp.CreatePhotoNegative();
-  //}
-  //catch (const std::runtime_error& err) {
-  //  std::cerr << err.what() << std::endl;
-  //}
   main_frame->Center();
   main_frame->Show();
   SetTopWindow(main_frame);
