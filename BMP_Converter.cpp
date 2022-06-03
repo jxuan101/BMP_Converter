232109ec8fce46c10273d84a389ca8b2d8811bb0
jxuan101 <35944722+jxuan101@users.noreply.github.com>
2022-06-02 22:00:07 -0400

    Modify variable name

--- BMP_Converter.cpp
+++ BMP_Converter.cpp
@@ -5,14 +5,15 @@
 
 
 int main() {
-  std::string file_name = "";
+  std::string file_path = "";
   std::cout << "Input file path (File must be in BMP format, 24 bits per pixel, uncompressed):" << std::endl;
-  std::cin >> file_name;
+  std::cin >> file_path;
   try {
-    BMP bmp(file_name);
+    BMP bmp(file_path);
     bmp.createPhotoNegative();
   }
   catch (const std::runtime_error &err) {
     std::cerr << err.what() << std::endl;
   }
+  std::cin >> file_path;
 }
